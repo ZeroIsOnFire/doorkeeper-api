13c1ef769ba3fe59e60469e1454a14c27dabc4b0
ZeroIsOnFire <juniorfachin@hotmail.com>
2020-02-11 21:48:24 -0300

    Adiciona rotas de api do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
+  scope module: :api, defaults: { format: :json }, path: 'api' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      devise_for :users, controllers: { registrations: 'api/v1/users/registrations' },
+                         skip: [:sessions, :password]
+    end
+  end
+
   devise_for :users
   resources :characters
   root to: 'characters#index'
