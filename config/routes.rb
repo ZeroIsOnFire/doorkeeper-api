99fdb996ea15b59815d1e2143932f292ae14145f
ZeroIsOnFire <juniorfachin@hotmail.com>
2020-02-11 21:50:24 -0300

    Instala doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  use_doorkeeper
   scope module: :api, defaults: { format: :json }, path: 'api' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       devise_for :users, controllers: { registrations: 'api/v1/users/registrations' },
